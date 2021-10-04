94228d0ced2b58ecc9a03f0620034860b6aecb77
Shreyas3-git <83245241+Shreyas3-git@users.noreply.github.com>
2021-10-04 17:26:19 +0530

    Adding EmployeePayroll-Application src

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PayrollheaderComponent } from './payrollheader/payrollheader.component';
+import { PayrollhomeComponent } from './payrollhome/payrollhome.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'details' , component: PayrollheaderComponent},
+  {path: 'addpayroll' , component: PayrollhomeComponent},
+  {path: '',   redirectTo:'details',  pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  
+ }
--- src/app/payrollhome/payrollhome.component.ts
+++ src/app/payrollhome/payrollhome.component.ts
@@ -8,8 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class PayrollhomeComponent implements OnInit {
 
   constructor() { }
-
+  isCheck : boolean = false;
   ngOnInit(): void {
   }
 
+
+  payrollHeader() : void {
+    this.isCheck = true;
+  }
 }
